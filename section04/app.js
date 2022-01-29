15e4c938cf8756a64a5e018fd4231a95a8c0a236
Darcee Quebe <darceerl@byui.edu>
2022-01-28 21:28:02 -0700

    Add CRUD functionality to product model using Mongoose

--- section04/app.js
+++ section04/app.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+//const User = require('./models/user');
 
 const app = express();
 
@@ -19,20 +20,24 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('61f468cc30971f0a7bf87416')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('61f468cc30971f0a7bf87416')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(PORT);
-});
+mongoose.connect(process.env.MONGODB_URL)
+  .then(result => {
+    app.listen(PORT);
+  })
+  .catch(err => {
+    console.log(err);
+  });
