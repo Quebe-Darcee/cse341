5ee11e9ae11e592a95d624ba69dfca988e9bf1f8
Darcee Quebe <darceerl@byui.edu>
2022-02-02 20:36:33 -0700

    Add session middleware and related fixes

--- section04/controllers/admin.js
+++ section04/controllers/admin.js
@@ -4,7 +4,8 @@ exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    editing: false
+    editing: false,
+    isAuthenticated: req.session.isLoggedIn
   });
 };
 
@@ -48,7 +49,8 @@ exports.getEditProduct = (req, res, next) => {
         pageTitle: 'Edit Product',
         path: '/admin/edit-product',
         editing: editMode,
-        product: product
+        product: product,
+        isAuthenticated: req.session.isLoggedIn
       });
     })
     .catch(err => console.log(err));
@@ -66,7 +68,7 @@ Product.findById(prodId).then(product => {
   product.price = updatedPrice;
   product.description = updatedDesc;
   product.imageUrl = updatedImageUrl;
-  return product.save()
+  return product.save();
 })
   .then(result => {
     console.log('UPDATED PRODUCT!');
@@ -83,7 +85,8 @@ exports.getProducts = (req, res, next) => {
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
-        path: '/admin/products'
+        path: '/admin/products',
+        isAuthenticated: req.session.isLoggedIn
       });
     })
     .catch(err => console.log(err));
