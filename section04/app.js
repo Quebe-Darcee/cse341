1f143a385a77f87f6457821360ff08cfaf017ce4
Darcee Quebe <darceerl@byui.edu>
2022-02-04 15:44:42 -0700

    Add route protection and csrf protection

--- section04/app.js
+++ section04/app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -16,6 +17,7 @@ const store = new MongoDBStore({
   uri: process.env.MONGODB_URL,
   collection: 'sessions'
 });
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -34,6 +36,7 @@ app.use(
     store: store
   })
 );
+app.use(csrfProtection);
 
 app.use((req, res, next) => {
   if (!req.session.user) {
@@ -45,7 +48,13 @@ app.use((req, res, next) => {
       next();
     })
     .catch(err => console.log(err));
-})
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -55,18 +64,6 @@ app.use(errorController.get404);
 
 mongoose.connect(process.env.MONGODB_URL)
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Darcee',
-          email: 'darcee@test.com',
-          cart:{
-            items:[]
-          }
-        });
-        user.save();
-      }
-    });
     app.listen(PORT);
   })
   .catch(err => {
